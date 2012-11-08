cb4953061a1d2224a33ed608dc70bec3f1d4c3ec
Ronny Wang <ronnywang@gmail.com>
2012-11-08 17:16:26 +0800

    加上基本的 framework init

---
+++ init.inc.php
@@ -0,0 +1,11 @@
+<?php
+
+set_include_path(
+    __DIR__ . '/libs/'
+    . PATH_SEPARATOR . __DIR__ . '/models'
+);
+
+include(__DIR__ . '/libs/Pix/Loader.php');
+Pix_Loader::registerAutoload();
+
+Pix_Table::setDefaultDb(new Pix_Table_Db_Adapter_Sqlite(__DIR__ . '/db.sqlite'));
